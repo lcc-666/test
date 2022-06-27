f2e58c38c7286d8730cdf7fb3ac823f596ac05d5
lcc-666 <2502247952@qq.com>
2022-06-27 18:56:21 +0800

    添加柱状图

--- view.py
+++ view.py
@@ -1,26 +1,60 @@
 import matplotlib.pyplot as plt
-import matplotlib
+import numpy as np
 
-"""2019 理科
-本科一批A段 507
-本科一批B段 507
-本科二批A段 447
-本科二批B段 434
-2020 理科
-本科一批A段 537
-本科二批A段 473
-本科二批B段 453
-2021 理科
-本科一批A段 505
-本科二批A段 446
-本科二批B段 431
-"""
-plt.rcParams['font.sans-serif']=['SimHei']
-x=["2019","2020","2021"]
-y=[507,537,505]
 
-plt.plot(x, y, "g", marker='D', markersize=5)
-plt.xlabel("年份")
-plt.ylabel("录取分数")
-plt.title("2019至2020年录取分数线")
-plt.show()
+def Histogram(grade_data):
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+
+    labels = ['2019', '2020', '2021']
+    a = grade_data[0]
+    b = grade_data[1]
+    c = grade_data[2]
+    d = grade_data[3]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.15  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width * 1.5, a, width, label='本科一批A段')
+    rects2 = ax.bar(x - width * 0.5, b, width, label='本科一批B段')
+    rects3 = ax.bar(x + width * 0.5, c, width, label='本科二批A段')
+    rects4 = ax.bar(x + width * 1.5, d, width, label='本科二批B段')
+
+    plt.yticks([400, 450, 500, 550, 600, 650, 700])
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom'
+                        )
+
+    autolabel(rects1)
+    autolabel(rects2)
+    autolabel(rects3)
+    autolabel(rects4)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('最低录取分数')
+    ax.set_title('2019到2021年最低录取分数线')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    date = [
+        [507, 537, 505],
+        [507, 0, 0],
+        [447, 473, 446],
+        [434, 453, 431],
+    ]
+    Histogram(date)
