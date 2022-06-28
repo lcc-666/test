3545ea856f001c40503e05f1c23f2192b7f14030
lcc-666 <2502247952@qq.com>
2022-06-28 11:40:52 +0800

    添加分数输出

--- get_grade.py
+++ get_grade.py
@@ -4,34 +4,34 @@ from numpy import transpose
 import view
 
 
-def get_num(school_id):
+def get_num(school_id, j):
     header = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 "
                       "Safari/537.36 Edg/101.0.1210.39 "
     }
-    dt = {1: "理科"}
+    dt = {1: "理科", 2: "文科"}
 
-    for j in dt.keys():
-        date = []
-        for i in range(2019, 2022):
-            url = "https://static-data.gaokao.cn/www/2.0/schoolprovinceindex/{}/{}/14/{}/1.json".format(i, school_id, j)
-            txt = requests.get(url=url, headers=header, ).json()
-            item = txt['data']["item"]
-            # print(item[0]["year"], dt[j])
-            dict = {
-                "本科一批A段": 0,
-                "本科一批B段": 0,
-                "本科二批A段": 0,
-                "本科二批B段": 0,
-            }
-            for one in item:
-                dict[one["local_batch_name"]] = int(one["min"])
+    date = []
+    for i in range(2019, 2022):
+        url = "https://static-data.gaokao.cn/www/2.0/schoolprovinceindex/{}/{}/14/{}/1.json".format(i, school_id, j)
+        txt = requests.get(url=url, headers=header, ).json()
+        item = txt['data']["item"]
+        print(item[0]["year"], dt[int(j)])
+        item_dict = {
+            "本科一批A段": 0,
+            "本科一批B段": 0,
+            "本科二批A段": 0,
+            "本科二批B段": 0,
+        }
+        for one in item:
+            print(one["local_batch_name"],int(one["min"]))
+            item_dict[one["local_batch_name"]] = int(one["min"])
 
-            date.append(list(dict.values()))
-            time.sleep(0.5)
-        transposed = transpose(date).tolist()
-        view.Histogram(transposed)
+        date.append(list(item_dict.values()))
+        time.sleep(0.5)
+    transposed = transpose(date).tolist()
+    view.Histogram(transposed)
 
 
 if __name__ == '__main__':
-    get_num(269)
+    get_num(269, 1)
